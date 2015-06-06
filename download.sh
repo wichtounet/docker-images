2f74b055ba463fc86bad1dd3adda35d33570091b
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-06-06 17:26:18 +0200

    Overcome dockerfile limitations

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Since dockerfiles are way too limited for remote files, it is necessary to use a script alongside...
+
+wget http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-hardened+nomultilib/stage3-amd64-hardened-20150604.tar.bz2 stage3-amd64-hardened.tar.bz2
