58b48e5dc87390210b205cdf1a757661870d84b4
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-06-08 19:58:18 +0200

    Download the correct file

--- gentoo-hardened/download.sh
+++ gentoo-hardened/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Since dockerfiles are way too limited for remote files, it is necessary to use a script alongside...
 
-wget http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-hardened+nomultilib/stage3-amd64-hardened-20150604.tar.bz2
+wget http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-hardened+nomultilib/stage3-amd64-hardened+nomultilib-20150604.tar.bz2 
 
-mv stage3-amd64-hardened-20150604.tar.bz2 stage3-amd64-hardened.tar.bz2
+mv stage3-amd64-hardened+nomultilib-20150604.tar.bz2 stage3-amd64-hardened.tar.bz2
